5c0a12d57cf94ce5efdc2583e4bd1dc71e94c59f
johnsoncodehk <johnsoncodehk@gmail.com>
2021-05-24 03:53:53 +0800

    feat: setup volar preview

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import * as volar from '@volar/experimental/client';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(volar.vuePlugin);
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as volar from '@volar/experimental/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(volar.vitePluginOptions)],
 })
