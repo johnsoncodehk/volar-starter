06654641def51cb178fc98235042cdf8833534ec
johnsoncodehk <johnsoncodehk@gmail.com>
2022-02-21 09:54:46 +0800

    chore: remove `@volar/experimental`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import * as volar from '@volar/experimental/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(volar.getVuePluginOptionsForVite())],
+  plugins: [vue()],
 })
