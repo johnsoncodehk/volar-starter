cc32c02397825acda0a0760d094fb7d54373b391
johnsoncodehk <johnsoncodehk@gmail.com>
2022-03-12 08:52:50 +0800

    feat: add prettier plugin example for volar

---
+++ volarPlugins/prettier-html.ts
@@ -0,0 +1,31 @@
+import type { EmbeddedLanguagePlugin } from '@volar/vue-language-service-types';
+import * as prettier from 'prettier';
+
+export default function (): EmbeddedLanguagePlugin {
+
+    return {
+
+        format(document, range, options) {
+
+            if (document.languageId !== 'html')
+                return;
+
+            const formattedText = prettier.format(document.getText(), {
+                tabWidth: options.tabSize,
+                useTabs: !options.insertSpaces,
+                parser: document.languageId,
+            });
+
+            if (formattedText === document.getText())
+                return []; // return empty array instead of undefined to avoid progress to next plugin
+
+            return [{
+                range: {
+                    start: document.positionAt(0),
+                    end: document.positionAt(document.getText().length),
+                },
+                newText: formattedText,
+            }];
+        },
+    }
+}
