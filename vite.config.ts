9ce7ac0c2a007aaad3d0819edd84c2cd179aa843
johnsoncodehk <johnsoncodehk@gmail.com>
2022-06-19 17:26:14 +0800

    chore: add vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Preview from 'vite-plugin-vue-component-preview';
+import Inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Inspect(),
     Preview(),
     vue(),
   ],
