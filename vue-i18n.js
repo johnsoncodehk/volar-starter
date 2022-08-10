cd34100ba39a1258198d7883c3d5887f6ea7bd1f
johnsoncodehk <johnsoncodehk@gmail.com>
2022-08-10 17:15:46 +0800

    feat: example for i18n plugin

---
+++ vue-i18n.js
@@ -0,0 +1,33 @@
+/** @type {import('@volar/vue-language-core').VueLanguagePlugin} */
+const plugin = () => {
+
+	return {
+
+		resolveEmbeddedFile(fileName, sfc, embeddedFile) {
+			if (embeddedFile.fileName.replace(fileName, '').match(/^\.(jsx|tsx)$/)) {
+				const keys = new Set();
+				for (const block of sfc.customBlocks) {
+					if (block.type === 'i18n') {
+						try {
+							const data = JSON.parse(block.content);
+							for (const [_, obj] of Object.entries(data)) {
+								for (const key in obj) {
+									keys.add(key);
+								}
+							}
+						}
+						catch { }
+					}
+				}
+				if (keys.size) {
+					embeddedFile.codeGen.addText(`
+						interface __VLS_Ctx {
+							$t(key: ${[...keys].map(key => `'${key}'`).join(' | ')}): void;
+						}
+					`);
+				}
+			}
+		},
+	};
+};
+module.exports = plugin;
