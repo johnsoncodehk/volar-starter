f9fc7d51d340b5dd8d92b833877f8cbc87f9d50c
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-20 21:13:18 +0800

    Update volar.config.js

--- volar.config.js
+++ volar.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     plugins: [
-        require('@volar-plugins/prettyhtml')({ printWidth: 100 }),
+        require('@volar-plugins/prettyhtml').default({ printWidth: 100 }),
     ],
 };
